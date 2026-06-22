b3e5c1a24bc016f345d5d10d0b005dad19949feb
robfrank <ro.franchini@gmail.com>
2026-06-22 15:39:32 +0200

    feat(spring-cluster): scaffold spring boot module

---
+++ spring-cluster/src/main/java/com/arcadedb/examples/springcluster/SpringClusterApplication.java
@@ -0,0 +1,12 @@
+package com.arcadedb.examples.springcluster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClusterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringClusterApplication.class, args);
+  }
+}
